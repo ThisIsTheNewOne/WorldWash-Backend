c2e4c842b8898fbdbc673b4ff9f0a9c12af67407
Paul Balta <baltapaul2@gmail.com>
2024-05-12 23:30:43 +0300

    adding the database for the back end project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common'; 
+
+const corsOptions = {
+  origin: ['http://localhost:3000', 'http://localhost:4000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  credentials: true, 
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const cors = require('cors'); 
+  
+  const port = process.env.PORT || 3000; 
+  await app.listen(port);
+  app.use(cors(corsOptions));
+
+  Logger.log(`NestJS app is running on http://localhost:${port}`, 'Bootstrap');
 }
 bootstrap();
